8fa7d06d53e6f321e2fa4a7d047cd366a66fbcf4
nikhileshr-lanatus <nikhilesh.r@lanatussystems.com>
2024-12-12 21:58:08 +0530

    Fix: Improve styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,36 +1,45 @@
-
 import { useState } from "react";
 import "./App.css";
 import { CreateTodo } from "./components/screens/CreateTodo";
 import { useGetTodos } from "./components/screens/hooks/useGetTodos";
 import { TodoList } from "./components/screens/Todos";
 import FilterTodos from "./components/screens/FilterTodos";
+import { Loader } from "lucide-react";
 
 function App() {
-  const { data: todos } = useGetTodos();
+  const { data: todos, isLoading } = useGetTodos();
   const [value, setValue] = useState("All");
-
 
   return (
     <div className="h-screen w-screen bg-base-100">
+      <div className="h-screen max-w-2xl  m-auto w-full p-2 flex flex-col">
+        <div>
+          <h1 className="text-3xl font-bold py-5">Todo App</h1>
+        </div>
+        <div className="flex gap-2">
+          <CreateTodo />
+          <FilterTodos value={value} onChange={setValue} />
+        </div>
 
-    <div className="h-screen max-w-lg m-auto w-full p-2 flex flex-col">
-      <div>
-        <h1 className="text-3xl font-bold py-5">Todo App</h1>
+        {isLoading ? (
+          <div className="w-full h-full flex justify-center items-center">
+            <Loader className="animate-spin" />
+          </div>
+        ) : (
+           <> {todos && (todos?.length === 0 ? (
+              <div className="w-full h-full flex justify-center items-center border rounded-sm mt-1">
+                <h6 className="text-center">No Todos Found</h6>
+              </div>
+            ) : (
+              <TodoList
+                todos={todos?.filter(
+                  (todo) => todo.status === value || value === "All"
+                )}
+              />
+            ))}
+            </>
+        )}
       </div>
-      <div className="flex gap-2">
-        <CreateTodo />
-        <FilterTodos value={value} onChange={setValue} />
-      </div>
-
-      {todos && (
-        <TodoList
-          todos={todos.filter(
-            (todo) => todo.status === value || value === "All"
-          )}
-        />
-      )}
-    </div>
     </div>
   );
 }
--- frontend/src/components/screens/Todos.tsx
+++ frontend/src/components/screens/Todos.tsx
@@ -11,46 +11,39 @@ export const TodoList = ({ todos }: { todos: Todo[] }) => {
 
   return (
     <div className="w-full h-full border rounded-md p-2 overflow-scroll">
-  
       <div className="flex flex-col gap-2">
         {todos?.map((todo) => (
           <div
             key={todo._id}
-            className="border hover:bg-accent rounded-md px-2 flex justify-between gap-2 items-start py-2"
+            className="border hover:bg-accent rounded-md px-2 flex flex-col justify-between gap-2 items-start py-2"
           >
-            <div className="flex flex-col h-auto flex-1 max-w-[60%] break-words items-start justify-center">
-              <div className="font-semibold text-left">
-                {todo.title}
+            <div className="flex h-auto flex-1 w-full break-words  justify-between items-center">
+              <div className="font-semibold text-left">{todo.title}</div>
+              <div className="flex gap-2 items-center">
+                <TodoSelector
+                  value={todo.status}
+                  onChange={(val) =>
+                    updateTodo({
+                      data: {
+                        status: val,
+                      },
+                      id: todo._id,
+                    })
+                  }
+                />
+                <Button
+                  size={"sm"}
+                  variant="destructive"
+                  className="w-max h-[30px]"
+                  onClick={() => {
+                    deleteTodo(todo._id);
+                  }}
+                >
+                  <Trash2 />
+                </Button>
               </div>
-              <div className="text-left flex-1">
-                {todo.description}
-              </div>
             </div>
-            <div className="text-left">
-              <TodoSelector
-                value={todo.status}
-                onChange={(val) =>
-                  updateTodo({
-                    data: {
-                      status: val,
-                    },
-                    id: todo._id,
-                  })
-                }
-              />
-            </div>
-            <div className="text-center">
-              <Button
-                size={'sm'}
-                variant="destructive"
-                className="w-max h-[30px]"
-                onClick={() => {
-                  deleteTodo(todo._id);
-                }}
-              >
-                <Trash2 />
-              </Button>
-            </div>
+            <div className="text-left flex-1">{todo.description}</div>
           </div>
         ))}
       </div>
