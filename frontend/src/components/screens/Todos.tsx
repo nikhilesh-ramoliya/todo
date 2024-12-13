fdda998a5d2943de1490872331b51488e794c7df
nikhileshr-lanatus <nikhilesh.r@lanatussystems.com>
2024-12-13 08:19:27 +0530

    Fix: loadings

--- frontend/src/components/screens/Todos.tsx
+++ frontend/src/components/screens/Todos.tsx
@@ -1,4 +1,4 @@
-import { Trash2 } from "lucide-react";
+import { Loader, Trash2 } from "lucide-react";
 import { Button } from "../ui/button";
 import { TodoSelector } from "../ui/todoStatusSelector";
 import { useDeleteTodo } from "./hooks/useDeleteTodo";
@@ -6,9 +6,6 @@ import { useUpdateTodo } from "./hooks/useUpdateTodo";
 import { Todo } from "./types/todoType";
 
 export const TodoList = ({ todos }: { todos: Todo[] }) => {
-  const { mutate: deleteTodo } = useDeleteTodo();
-  const { mutate: updateTodo } = useUpdateTodo();
-
   return (
     <div className="w-full h-full border rounded-md p-2 overflow-scroll">
       <div className="flex flex-col gap-2">
@@ -20,27 +17,8 @@ export const TodoList = ({ todos }: { todos: Todo[] }) => {
             <div className="flex h-auto flex-1 w-full break-words  justify-between items-center">
               <div className="font-semibold text-left">{todo.title}</div>
               <div className="flex gap-2 items-center">
-                <TodoSelector
-                  value={todo.status}
-                  onChange={(val) =>
-                    updateTodo({
-                      data: {
-                        status: val,
-                      },
-                      id: todo._id,
-                    })
-                  }
-                />
-                <Button
-                  size={"sm"}
-                  variant="destructive"
-                  className="w-max h-[30px]"
-                  onClick={() => {
-                    deleteTodo(todo._id);
-                  }}
-                >
-                  <Trash2 />
-                </Button>
+                <UpdateTodo todo={todo} />
+                <DeleteTodo todo={todo} />
               </div>
             </div>
             <div className="text-left flex-1">{todo.description}</div>
@@ -48,5 +26,43 @@ export const TodoList = ({ todos }: { todos: Todo[] }) => {
         ))}
       </div>
     </div>
+  );
+};
+
+const DeleteTodo = ({ todo }: { todo: Todo }) => {
+  const { mutate: deleteTodo, isPending: isLoading } = useDeleteTodo();
+
+  return (
+    <Button
+      size={"sm"}
+      variant="destructive"
+      className="w-max h-[30px]"
+      onClick={() => {
+        deleteTodo(todo._id);
+      }}
+    >
+      {isLoading ? <Loader className="animate-spin" size="sm" /> : <Trash2 />}
+    </Button>
+  );
+};
+
+const UpdateTodo = ({ todo }: { todo: Todo }) => {
+  const { mutate: updateTodo, isPending: isLoadingUpdate } = useUpdateTodo();
+
+  return (
+    <>
+      <TodoSelector
+        disabled={isLoadingUpdate}
+        value={todo.status}
+        onChange={(val) =>
+          updateTodo({
+            data: {
+              status: val,
+            },
+            id: todo._id,
+          })
+        }
+      />
+    </>
   );
 };
